0b1c26ff52bac1e1085b603077b026080306c93f
jawad52923 <jawadahmedshkh52923@gmail.com>
2022-11-12 17:19:32 +0500

    chore: created register, login apis

--- appRoutes/user.js
+++ appRoutes/user.js
@@ -1,34 +1,18 @@
 const Router = require("express").Router();
-const requireAuth = require("../middlewares/auth");
+const requireAuth = require("../middleware/auth");
 const {
     getUsers,
-    addUser,
     getParticularUser,
     deleteUser,
-    updateUser,
 } = require("../controllers/user");
-const { ROLES } = require("../utils/constant");
 
 Router.get(
     "/",
-    [requireAuth],
     getUsers
 );
 
 Router.get("/:id", [requireAuth], getParticularUser);
 
-
-Router.post(
-    "/",
-    [requireAuth],
-    addUser
-);
-
-Router.put(
-    "/:id",
-    [requireAuth],
-    updateUser
-);
 
 Router.delete(
     "/:id",
--- routes.js
+++ routes.js
@@ -3,9 +3,16 @@ const Router = require("express").Router();
 
 const {
     logIn,
- 
+    register
 } = require("./controllers/user");
 
+const userRoutes = require("./appRoutes/user")
+
+
 Router.post("/login", logIn);
+Router.post("/register", register);
+
+
+Router.use('/user',userRoutes)
 
 module.exports = Router;
