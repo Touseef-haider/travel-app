be474faf5ce9e7f43899e4c92f62ce3a48b6ac5a
touseef-haider <touseefhaider355@gmail.com>
2022-12-24 18:04:26 +0500

    chore: done wuth the orifu

--- models/profile.js
+++ models/profile.js
@@ -5,31 +5,24 @@ const Schema = mongoose.Schema;
 const profileSchema = new Schema({
   first_name: {
     type: String,
-    required: true,
   },
   last_name: {
     type: String,
-    required: true,
   },
   address: {
     type: String,
-    required: true,
   },
   phone: {
     type: String,
-    required: true,
   },
   city: {
     type: String,
-    required: true,
   },
   state: {
     type: String,
-    required: true,
   },
   zip: {
     type: String,
-    required: true,
   },
   user: {
     type: mongoose.Types.ObjectId,
