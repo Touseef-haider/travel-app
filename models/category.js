4a0a26f9de836bfdcfb53fb8bf4d9d4c44f091c1
touseef-haider <touseefhaider355@gmail.com>
2023-01-20 00:43:26 +0500

    chore: added image key in category

--- models/category.js
+++ models/category.js
@@ -3,6 +3,11 @@ const mongoose = require("mongoose");
 const categorySchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
   },
 });
 
